b06ffcc4c4f04c3db4a92597a11b8d7dbe8da7cf
Ranohavison <ranohavison399@gmail.com>
2025-07-17 19:26:33 +0300

    New update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         rel="stylesheet"
       />
     </head>
-    <body className="bg-ocean-950 text-white antialiased font-serif text-md sm:text-xl">
+    <body className="bg-ocean-950 text-white antialiased font-serif text-sm xs:text-md sm:text-xl">
     {children}
     </body>
     </html>
